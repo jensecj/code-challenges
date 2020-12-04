37a7bcf54d310dfe035112fcb71967756c8cd211
Jens C. Jensen <jens@subst.net>
2020-12-04 11:26:41 +0100

    solved aoc 2020 day 1 in clojure

---
+++ advent-of-code-2020/01/clj/main.clj
@@ -0,0 +1,39 @@
+(ns main
+  (:require [clojure.string :as str]))
+
+(defn read_input []
+  (as-> (slurp "../input.in") i
+    (str/split i #"\n")
+    (map #(Integer/parseInt %) i)))
+
+(defn part1 [input]
+  (for [a input
+        b input
+        :when (< a b)
+        :when (= (+ a b) 2020)]
+    (list a b (* a b))))
+
+(defn part2 [input]
+  (for [a input
+        b input
+        c input
+        :when (< a b c)
+        :when (= (+ a b c) 2020)]
+    (list a b c (* a b c))))
+
+(defn three_sum
+  "Quadratic solution to the 3SUM problem"
+  [input n]
+  (let [s (set input)]
+    (for [a input
+          b input
+          :let [c (- n (+ a b))]
+          :when (< a b c)
+          :when (s c)]
+      (list a b c (* a b c)))))
+
+(let [input (read_input)]
+  (println (part1 input))
+  (println (part2 input))
+  (println (three_sum input 2020)))
+
