020de1d71eb22c295f444e094440b774371d4d3c
Jens C. Jensen <jens@subst.net>
2020-12-09 14:50:51 +0100

    aoc 2020 day 7: use regexes to parse input instead; benchmark solutions; add some tests

--- advent-of-code-2020/07/main.py
+++ advent-of-code-2020/07/main.py
@@ -1,98 +1,91 @@
+import sys
+import re
 from collections import deque
+from compytetive.util import time
 
 
 def read_input(filename):
     with open(filename) as f:
         data = f.readlines()
 
-    for d in data:
-        # split lines into (container, children)
-        split = d.split("contain")
-
-        # normalize the bag names
-        split = [
-            s.replace("bags.", "bag")
-            .replace("bags", "bag")
-            .replace("bag.", "bag")
-            .strip()
-            for s in split
-        ]
-
-        lhs = split[0].strip()  # the container bag
-        rhs = [s.strip() for s in split[1].split(",")]  # the bags that it contains
-
-        # indicate the bags which can hold no other bags
-        rhs = rhs if "no other bag" not in rhs else None
-
-        # convert bags representation into tuples of (bag, count) where count is the number of
-        # times that the bag occurs
-        rhs = [(s[2:], int(s[0])) for s in rhs] if rhs else None
-
-        yield lhs, rhs
-
-
-def bag_in_children(bag, cdr):
-    return bag in [k for k, _ in cdr]
-
-
-def contains_bag(bag, all_bags):
-    """Return the subset of ALL_BAGS which contain BAG"""
-    bags = []
-    for k, v in all_bags:
-        if v and bag_in_children(bag, v):
-            bags.append(k)
+    bags = {}
+    for b in data:
+        bag = re.search(r"(.+?) bag", b).group(1)
+        contains = re.findall(r"(\d+) (.+?) bag", b)
+        bags[bag] = [(int(c), b) for c, b in contains]
 
     return bags
 
 
-def part1(data):
+def contains_bag(bag, bags):
+    """Return the subset of BAGS which contain BAG"""
+    contains = []
+    for b, children in bags.items():
+        if bag in [c for _, c in children]:
+            contains.append(b)
+
+    return contains
+
+
+@time
+def part1(bags):
     """
-    To find the set of bags which, in any way, contain a shiny gold bag,
-    we start at the shiny gold bag and move backwards, and lookup all bags which contain it,
-    then we look up all the bags that contain those bags, etc.
-    keeping track of the bags we have seen.
+    To find the set of bags which contain a shiny gold bag, directly or indirectly, we start at
+    the shiny gold bag and move backwards, and lookup all bags which contain it, then we look up
+    all the bags that contain those bags, etc. While keeping track of the bags we have seen.
     """
     seen = set()
-    to_check = deque(["shiny gold bag"])
+    to_check = deque(["shiny gold"])
+
     while to_check:
         b = to_check.pop()
-        for bb in contains_bag(b, data):
+
+        for bb in contains_bag(b, bags):
             seen.add(bb)
             to_check.append(bb)
 
     return len(seen)
 
 
-def part2(data):
+@time
+def part2(bags):
     """
     To find the number of bags that a bag contain, we start at our shiny gold bag, then iterate
     down through all the bags it contains, then the bags those bags contain, all while keeping
     track of the factor between the current bag and its parent, which we add to an accumulator.
     """
-    d = dict(data)
-
-    to_check = deque([("shiny gold bag", 1)])
-
+    to_check = deque([(1, "shiny gold")])
     acc = 0
-    while to_check:
-        k, v = to_check.pop()
-        # print(f"current: {k}")
 
-        if e := d.get(k):
-            for bb, bv in e:
-                # print(f"contains {bv} {bb}")
+    while to_check:
+        v, b = to_check.pop()
+
+        if e := bags.get(b):
+            for bv, bb in e:
                 factor = v * bv
                 acc += factor
-                to_check.append((bb, factor))
+                to_check.append((factor, bb))
 
     return acc
 
 
 def main():
-    data = list(read_input("input.in"))
+    data = read_input("input.in")
     print(part1(data))
     print(part2(data))
 
 
 if __name__ == "__main__":
     main()
+
+
+real_input = read_input("input.in")
+test1_input = read_input("test1.in")
+test2_input = read_input("test2.in")
+
+print("\ntesting...", file=sys.stderr)
+assert part1(test1_input) == 4
+assert part1(real_input) == 287
+assert part2(test1_input) == 32
+assert part2(test2_input) == 126
+assert part2(real_input) == 48160
