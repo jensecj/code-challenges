c405273a4d3f4cdaf35fbd32531890d9e58551fd
Jens C. Jensen <jens@subst.net>
2020-12-12 12:55:51 +0100

    solved aoc 2020 day 12 in python

---
+++ advent-of-code-2020/12/main.py
@@ -0,0 +1,152 @@
+from typing import List, Tuple
+
+from compytetive.algorithms import manhatten_dist
+from compytetive.util import time
+
+
+Point = Tuple[int, int]
+Command = Tuple[str, int]
+Program = List[Command]
+Direction = str
+
+DIRECTIONS = ["N", "E", "S", "W"]
+
+
+def read_input(filename):
+    with open(filename) as f:
+        data = f.readlines()
+        data = [(s[0], s[1:].strip()) for s in data]
+        data = [(a, int(b)) for a, b in data]
+    return data
+
+
+def translate_move(heading: Direction, direction: Direction, value: int) -> Point:
+    assert heading in DIRECTIONS
+    assert direction in DIRECTIONS + ["F"]
+
+    if direction == "F":
+        direction = heading
+
+    if direction == "N":
+        return (0, value)
+    if direction == "E":
+        return (value, 0)
+    if direction == "S":
+        return (0, -value)
+    if direction == "W":
+        return (-value, 0)
+
+    raise Exception("Unable to translate move")
+
+
+assert translate_move("N", "F", 1) == translate_move("N", "N", 1) == (0, 1)
+assert translate_move("N", "E", 1) == translate_move("E", "F", 1) == (1, 0)
+assert translate_move("N", "S", 1) == (0, -1)
+assert translate_move("N", "W", 1) == (-1, 0)
+
+
+def change_direction(heading: Direction, direction: Direction, value) -> Direction:
+    assert heading in DIRECTIONS
+    assert direction in ["L", "R"]
+    assert value % 90 == 0
+
+    idx = DIRECTIONS.index(heading)
+    turn = value // 90
+
+    if direction == "L":
+        turn = -turn
+
+    return DIRECTIONS[(idx + turn) % len(DIRECTIONS)]
+
+
+assert change_direction("N", "L", 90) == "W"
+assert change_direction("N", "L", 180) == "S"
+assert change_direction("W", "L", 90) == "S"
+assert change_direction("S", "L", 90) == "E"
+assert change_direction("E", "L", 90) == "N"
+
+assert change_direction("N", "R", 90) == "E"
+assert change_direction("N", "R", 360) == "N"
+assert change_direction("N", "R", 180) == "S"
+assert change_direction("E", "R", 90) == "S"
+assert change_direction("S", "R", 90) == "W"
+assert change_direction("W", "R", 90) == "N"
+
+
+@time
+def part1(program):
+    direction = "E"
+    x, y = (0, 0)
+
+    for cmd, value in program:
+        if cmd in ["L", "R"]:
+            direction = change_direction(direction, cmd, value)
+        elif cmd in ["F", "N", "E", "S", "W", "B"]:
+            nx, ny = translate_move(direction, cmd, value)
+            x, y = x + nx, y + ny
+
+    return manhatten_dist((0, 0), (x, y))
+
+
+def rotate_point(point: Point, direction: Direction, value: int) -> Point:
+    x, y = point
+
+    if direction in "L":
+        for _ in range(value // 90):
+            x, y = -y, x
+
+    elif direction == "R":
+        for _ in range(value // 90):
+            x, y = y, -x
+
+    return x, y
+
+
+assert (
+    rotate_point((10, 4), "R", 0)
+    == rotate_point((10, 4), "L", 0)
+    == rotate_point((10, 4), "R", 360)
+    == rotate_point((10, 4), "L", 360)
+    == (10, 4)
+)
+
+assert rotate_point((10, 4), "R", 90) == rotate_point((10, 4), "L", 270) == (4, -10)
+assert rotate_point((10, 4), "R", 180) == rotate_point((10, 4), "L", 180) == (-10, -4)
+assert rotate_point((10, 4), "R", 270) == rotate_point((10, 4), "L", 90) == (-4, 10)
+
+
+@time
+def part2(program):
+    x, y = (0, 0)
+    wx, wy = (10, 1)
+    direction = "E"  # never changes in part2
+
+    for cmd, value in program:
+        if cmd in ["L", "R"]:
+            wx, wy = rotate_point((wx, wy), cmd, value)
+        elif cmd in DIRECTIONS:
+            nx, ny = translate_move(direction, cmd, value)
+            wx, wy = wx + nx, wy + ny
+        elif cmd == "F":
+            x, y = x + value * wx, y + value * wy
+
+    return manhatten_dist((0, 0), (x, y))
+
+
+def main():
+    data = read_input("input.in")
+    print(part1(data))
+    print(part2(data))
+
+
+if __name__ == "__main__":
+    main()
+
+
+test1_input = read_input("test1.in")
+assert part1(test1_input) == 25
+assert part2(test1_input) == 286
+
+real_input = read_input("input.in")
+assert part1(real_input) == 1294
+assert part2(real_input) == 20592
